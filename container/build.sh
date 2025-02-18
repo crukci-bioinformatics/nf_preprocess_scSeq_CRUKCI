617a6a6c6d86a25585888044e354b85890ff1522
Ashley Sawle <ads2202cu@gmail.com>
2025-02-18 10:10:41 +0000

    Update version

--- container/build.sh
+++ container/build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Version is the version of CellRanger in the container.
-version=8.0.1
+version=9.0.1
 
-sudo docker build --tag cruk_ci_preprocess_scSeq:${version} .
-singularity build cruk_ci_preprocess_scSeq-${version}.sif docker-daemon://cruk_ci_preprocess_scSeq:${version}
+sudo docker build --tag cruk_ci_preprocess_scseq:${version} .
+sudo singularity build cruk_ci_preprocess_scseq-${version}.sif docker-daemon://cruk_ci_preprocess_scseq:${version}
